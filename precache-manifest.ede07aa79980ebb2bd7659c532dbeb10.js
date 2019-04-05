a9526c54de4e828a49b9deb0bc3d6b7311f0aab4
saahilshaikh <44947415+saahilshaikh@users.noreply.github.com>
2019-04-05 21:53:24 +0530

    Updates

---
+++ precache-manifest.ede07aa79980ebb2bd7659c532dbeb10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4177ae5d1b8f1caf9fc8",
+    "url": "/reminder/static/js/runtime~main.88718075.js"
+  },
+  {
+    "revision": "0a5a47284ddefff2a1ec",
+    "url": "/reminder/static/js/main.ca0852fb.chunk.js"
+  },
+  {
+    "revision": "86ef55bededb4a6d58e1",
+    "url": "/reminder/static/js/2.f208391f.chunk.js"
+  },
+  {
+    "revision": "0a5a47284ddefff2a1ec",
+    "url": "/reminder/static/css/main.4e0980db.chunk.css"
+  },
+  {
+    "revision": "4d542326db9b1f382dc0b7acd4e64ff1",
+    "url": "/reminder/index.html"
+  }
+];
